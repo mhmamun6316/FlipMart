8ac2f4476b0867da9504bbd303f0df6be6a0bf3d
mhmamun6316 <mhmamun29404@gmail.com>
2021-10-29 15:24:05 +0600

    multiAuth

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,11 +22,16 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            foreach ($guards as $guard) {
+                if (Auth::guard($guard)->check() && Auth::user()->role->id == 1 ) {
+                    return redirect()->route('admin.dashboard');
+        
+                }elseif (Auth::guard($guard)->check() && Auth::user()->role->id == 2) {
+                    return redirect()->route('user.dashboard');
+                }else{
+                    return $next($request);
+                } 
             }
         }
-
-        return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
+Use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix'=>'admin','middleware' =>['admin','auth'],'namespace'=>'Admin'], function(){
+    Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+});
+
+Route::group(['prefix'=>'user','middleware' =>['user','auth'],'namespace'=>'User'], function(){
+    Route::get('dashboard',[UserController::class,'index'])->name('user.dashboard');
 });
